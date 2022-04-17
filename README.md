13033cb1e0ea0f3774822389d242a0ae830e93e0
Jonathan Bartlett <jonathan@bartlettpublishing.com>
2022-04-17 10:31:55 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kubernetes Example Frontend
+
+This is the example frontend used in the upcoming book, "Cloud Native Applications: Zero to Superhero" by Jonathan Bartlett.
---
+++ app.js
@@ -0,0 +1,27 @@
+let basePath = "/api";
+function submitform(evt) {
+    evt.preventDefault();
+    let data = new FormData(evt.target);
+    let title = data.get("title");
+    let message = data.get("message");
+    fetch(basePath + "/messages/create", {
+        method: "POST",
+        body: `title=${title}&message=${message}`
+    }).then(result => {
+        reloadMessages();
+    })
+}
+function reloadMessages() {
+    let listElement = document.getElementById("messages");
+    fetch(basePath + "/messages/list").then(result => {
+        result.json().then(data => {
+            data.forEach(msg => {
+                let e = document.createElement("li");
+                e.innerText = `${msg.title}: ${msg.message}`;
+                listElement.appendChild(e);
+            })
+        });
+    });
+    console.log(element);
+}
+window.onload = reloadMessages;
